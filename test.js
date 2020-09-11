25d70e7ab5e4bb69f3fc94716e8d90df1521c8d4
Jules Prevost <jprevostdev@gmail.com>
2020-09-11 17:07:08 +0200

    working but not as script

--- test.js
+++ test.js
@@ -9,3 +9,24 @@ console.log(description);
 const url = window.location["href"];
 
 console.log(url);
+
+let createResource = () => {
+fetch("http://localhost:3000/api/v1/resources", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "X-User_Email": "riri@gmail.com",
+      "X-User-Token": "tsEf2aitUyHW2YjT7KqE",
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify( { "resource": { "title": title, "description": description, "url": url } })
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+createResource();
+
+
