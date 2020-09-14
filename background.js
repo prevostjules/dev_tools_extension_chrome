99fc4936639f0addf0a04a6a3494120ecaedd268
Jules Prevost <jprevostdev@gmail.com>
2020-09-14 14:04:06 +0200

    on peut désormais entré la clé api directement via la popup de l'extension

--- background.js
+++ background.js
@@ -1,8 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
- var button = document.getElementById("call_api")
+ const button = document.getElementById("call_api");
 
- button.addEventListener("click", (e) => {
-   chrome.tabs.executeScript({ file: "call_api.js" })
- })
+ // button.addEventListener("click", (e) => {
+ //   chrome.tabs.executeScript({ file: "call_api.js" })
+ // })
+  const btnApiKey = document.getElementById("submit_api_key");
+
+  function callApi(apiKey) {
+   chrome.tabs.executeScript(
+     { code: "var apiKey = " + JSON.stringify(apiKey) },
+     function() {
+       chrome.tabs.executeScript({ file: "call_api.js" })
+     }
+   )
+  }
+
+  btnApiKey.addEventListener("click", (e) => {
+    const apiKey = document.getElementById("api-key").value;
+    callApi(apiKey);
+  })
+
+  // function callApi(options) {
+  //  chrome.tabs.executeScript(
+  //    { code: "var options = " + JSON.stringify(options) },
+  //    function() {
+  //      chrome.tabs.executeScript({ file: "call_api.js" })
+  //    }
+  //  )
+  // }
+
+  // button.addEventListener("click", (e) => {
+  //   const test = "ttttt"
+  //   callApi(test);
+  // })
+
+
 
 })
--- call_api.js
+++ call_api.js
@@ -9,21 +9,23 @@ const description = document.querySelector('meta[name="description"]').content;
 console.log(description);
 
 
-const createResource = () => {
-  fetch("http://localhost:3000/api/v1/resources", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-User_Email": "riri@gmail.com",
-      "X-User-Token": "1Ykek55eaUAeqrwX_-HH",
-    },
-    body: JSON.stringify({ resource:{ title: title, description: description, url: url } })
-  })
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data); // Look at local_names.default
-    });
-};
 
-createResource();
+
+// const createResource = () => {
+//   fetch("http://localhost:3000/api/v1/resources", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "X-User_Email": "riri@gmail.com",
+//       "X-User-Token": apiKey,
+//     },
+//     body: JSON.stringify({ resource:{ title: title, description: description, url: url } })
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       console.log(data); // Look at local_names.default
+//     });
+// };
+
+// createResource();
 
