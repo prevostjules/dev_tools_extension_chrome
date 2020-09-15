055633031df2020e5c2b28a4089d779a9b870ed6
Jules Prevost <jprevostdev@gmail.com>
2020-09-15 16:07:39 +0200

    the extension now grab the img

--- call_api.js
+++ call_api.js
@@ -1,17 +1,17 @@
 const createResource = () => {
-  fetch("https://my-dev-tools-links.herokuapp.com/api/v1/resources", {
+  fetch("http://localhost:3000//api/v1/resources", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "X-User_Email": resourceInfos.credentials.email,
       "X-User-Token": resourceInfos.credentials.apiKey,
     },
-    body: JSON.stringify({ resource:{ title: resourceInfos.title, description: resourceInfos.description, url: resourceInfos.url } })
+    body: JSON.stringify({ resource:{ title: resourceInfos.title, description: resourceInfos.description, url: resourceInfos.url }, photo: { imgUrl: resourceInfos.imgUrl } } )
   })
     .then(response => response.json())
     .then((data) => {
       console.log(data);
-      chrome.runtime.sendMessage({message: data, type: 'from_call_api'})
+      // chrome.runtime.sendMessage({message: data, type: 'from_call_api'})
     });
 };
 
--- get_infos.js
+++ get_infos.js
@@ -2,6 +2,14 @@ let title = document.querySelector("title").innerHTML;
 
 let url = window.location["href"];
 
+let imgUrl = "";
+
+if (document.querySelector('meta[property="og:image"]').content == null) {
+  imgUrl = ""
+} else {
+  imgUrl = document.querySelector('meta[property="og:image"]').content;
+}
+
 let description = "";
 if (document.querySelector('meta[name="description"]') == null) {
   description = "This website has no description yet."
@@ -9,5 +17,5 @@ if (document.querySelector('meta[name="description"]') == null) {
   description = document.querySelector('meta[name="description"]').content;
 }
 
-websiteInfos = {title: title, url: url, description: description};
+websiteInfos = {title: title, url: url, description: description, imgUrl: imgUrl};
 chrome.runtime.sendMessage({message: websiteInfos, type: 'website_infos'})
