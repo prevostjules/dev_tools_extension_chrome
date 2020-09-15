911769b3c0af26e7b7e9ce29d7be7fbd5ed6b613
Jules Prevost <jprevostdev@gmail.com>
2020-09-15 14:34:13 +0200

    suppression of comments

--- call_api.js
+++ call_api.js
@@ -1,23 +1,3 @@
-// let title = document.querySelector("title").innerHTML;
-// console.log(title);
-
-// let url = window.location["href"];
-
-// console.log(url);
-
-// let description = "";
-// if (document.querySelector('meta[name="description"]') == null) {
-//   description = "This website has no description yet."
-// } else {
-//   description = document.querySelector('meta[name="description"]').content;
-// }
-
-// console.log(description);
-
-// websiteInfos = {title: title, url: url, description: description};
-// chrome.runtime.sendMessage({message: websiteInfos, type: 'website_infos'})
-
-
 const createResource = () => {
   fetch("https://my-dev-tools-links.herokuapp.com/api/v1/resources", {
     method: "POST",
